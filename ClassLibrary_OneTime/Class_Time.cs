407b68e4fcb942d4d6ba2764ecac6b5c8a732142
jonathan198344 <jonathan_-1983@outlook.com>
2025-09-07 19:39:09 -0500

    using1

--- ClassLibrary_OneTime/Class_Time.cs
+++ ClassLibrary_OneTime/Class_Time.cs
@@ -1,21 +1,36 @@
 ﻿using System;
 
-namespace ClassLibrary_OneTime
+class Program
 {
-    public class Time
+    static void Main(string[] args)
     {
-        private int hours, minutes, seconds, milliseconds;
-        private bool isValid;
+        try
+        {
+            Time t1 = new Time();
+            Time t2 = new Time(5);
+            Time t3 = new Time(5, 10);
+            Time t4 = new Time(5, 10, 34);
+            Time t5 = new Time(23, 58, 34, 666);
 
-        
-        public Time() : this(0, 0, 0, 0) { }
+            Time[] tiempos = { t1, t2, t3, t4, t5 };
 
-       
-        public Time(int hours) : this(hours, 0, 0, 0) { }
+            Console.WriteLine("Tiempos iniciales:");
+            foreach (var t in tiempos)
+            {
+                Console.WriteLine(t.ToString());
+            }
 
-        public Time(int hours, int minutes) : this(hours, minutes, 0, 0) { }
-
-        
-        public Time(int hours, int minutes, int seconds) : this(hours, minutes, seconds, 0) { }
-
-        
+            Console.WriteLine("\nSumando cada tiempo con t3:");
+            foreach (var t in tiempos)
+            {
+                var suma = t.Add(t3);
+                bool pasaOtroDia = suma.IsOtherDay(t4);
+                Console.WriteLine($"{t.ToString()} + {t3.ToString()} = {suma.ToString()} - ¿Pasa al otro día con t4? {pasaOtroDia}");
+            }
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("Error: " + e.Message);
+        }
+    }
+}
--- ConsoleApp_OneTime2/Program.cs
+++ ConsoleApp_OneTime2/Program.cs
@@ -2,81 +2,39 @@
 
 using System;
 
-namespace ClassLibrary_OneTime
+class Program
 {
-    public class Time
+    static void Main(string[] args)
     {
-        private int hours, minutes, seconds, milliseconds;
-        private bool isValid;
-
-        // 1) sin parámetros
-        public Time() : this(0, 0, 0, 0) { }
-
-        // 2) con horas
-        public Time(int hours) : this(hours, 0, 0, 0) { }
-
-        // 3) con horas y minutos
-        public Time(int hours, int minutes) : this(hours, minutes, 0, 0) { }
-
-        // 4) con horas, minutos y segundos
-        public Time(int hours, int minutes, int seconds) : this(hours, minutes, seconds, 0) { }
-
-        // 5) con horas, minutos, segundos y milisegundos
-        public Time(int hours, int minutes, int seconds, int milliseconds)
+        try
         {
-            if (hours < 0 || hours > 23 ||
-                minutes < 0 || minutes > 59 ||
-                seconds < 0 || seconds > 59 ||
-                milliseconds < 0 || milliseconds > 999)
-                throw new ArgumentOutOfRangeException("Hora no válida");
+            Time t1 = new Time();
+            Time t2 = new Time(5);
+            Time t3 = new Time(5, 10);
+            Time t4 = new Time(5, 10, 34);
+            Time t5 = new Time(23, 58, 34, 666);
 
-            this.hours = hours;
-            this.minutes = minutes;
-            this.seconds = seconds;
-            this.milliseconds = milliseconds;
-            this.isValid = true;
+            Time[] tiempos = { t1, t2, t3, t4, t5 };
+
+            Console.WriteLine("Tiempos iniciales:");
+            foreach (var t in tiempos)
+            {
+                Console.WriteLine(t.ToString());
+            }
+
+            Console.WriteLine("\nSumando cada tiempo con t3:");
+            foreach (var t in tiempos)
+            {
+                var suma = t.Add(t3);
+                bool pasaOtroDia = suma.IsOtherDay(t4);
+                Console.WriteLine($"{t.ToString()} + {t3.ToString()} = {suma.ToString()} - ¿Pasa al otro día con t4? {pasaOtroDia}");
+            }
         }
-
-        public long ToMilliseconds() =>
-            ((hours * 3600L + minutes * 60L + seconds) * 1000L) + milliseconds;
-
-        public long ToSeconds() =>
-            (hours * 3600L) + (minutes * 60L) + seconds;
-
-        public long ToMinutes() =>
-            (hours * 60L) + minutes;
-
-        public bool IsOtherDay(Time other)
+        catch (Exception e)
         {
-            long total = this.ToMilliseconds() + other.ToMilliseconds();
-            return total >= 24L * 3600 * 1000;
-        }
-
-        public Time Add(Time other)
-        {
-            int ms = milliseconds + other.milliseconds;
-            int carryS = ms / 1000; ms %= 1000;
-
-            int s = seconds + other.seconds + carryS;
-            int carryM = s / 60; s %= 60;
-
-            int m = minutes + other.minutes + carryM;
-            int carryH = m / 60; m %= 60;
-
-            int h = (hours + other.hours + carryH) % 24;
-
-            return new Time(h, m, s, ms);
-        }
-
-        public override string ToString()
-        {
-            var dt = new DateTime(1, 1, 1, hours, minutes, seconds, milliseconds);
-            return dt.ToString("hh:mm:ss.fff tt");
+            Console.WriteLine("Error: " + e.Message);
         }
     }
 }
-
-
-
 
 
